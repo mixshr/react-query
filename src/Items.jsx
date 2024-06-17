a61f52aa0cb2e16bb2c0bbbf96bd44aa49ee1d09
mihailsarenko <mixail.sharenko@gmail.com>
2024-06-17 13:37:20 +0500

    commit

--- src/Items.jsx
+++ src/Items.jsx
@@ -2,13 +2,23 @@ import SingleItem from './SingleItem';
 import {useQuery} from "@tanstack/react-query";
 import customFetch from "./utils.js";
 const Items = ({ items }) => {
-  const result = useQuery({
-    queryKey: ['tasks'], queryFn: () => customFetch.get('/')
+  const {isLoading, data, error, isError} = useQuery({
+    queryKey: ['tasks'], queryFn: async () => {
+      const {data} = await customFetch.get('/')
+      return data;
+    }
   });
-  console.log(result);
+
+  if (isLoading) {
+    return <p style={{margin:'1rem'}}>Loading...</p>
+  }
+
+  if (isError) {
+    return <p style={{margin:'1rem'}}>Error...</p>
+  }
   return (
     <div className='items'>
-      {items.map((item) => {
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
